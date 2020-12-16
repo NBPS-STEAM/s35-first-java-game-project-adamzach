b70528c0841ad7be656fb1c784c9962585cf6395
adamzach <amzach0826@gmail.com>
2020-12-16 16:32:08 +0000

    Started coding

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello! In this game, you have to guess the secret number! Let's start!");
     }
 }
