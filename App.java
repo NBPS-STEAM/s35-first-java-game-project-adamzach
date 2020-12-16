fb73ba44c86529bf82ccfebcde9ae36906ba420b
adamzach <amzach0826@gmail.com>
2020-12-16 17:10:56 +0000

    Added most of the code of the first part

--- App.java
+++ App.java
@@ -1,5 +1,24 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello! In this game, you have to guess the secret number! Let's start!");
+        int number1 = 7;
+        int guess1 = 0;
+        while (guess1 != number1) 
+            {
+              guess1 = readInt("Guess the number between 1-10: ");
+            if ((number1 <= -1)&&(number1 >= 11))
+            {
+              System.out.println("I said a number 1-10...");
+            }
+            else if (guess1 > number1) 
+                {
+                System.out.println("Too high! Try again!");
+                }
+            else if (guess1 < number1)
+                {
+                  System.out.print("Too low! Try Again!");
+                }   
+        System.out.println("Correct!");
+        }
     }
 }
