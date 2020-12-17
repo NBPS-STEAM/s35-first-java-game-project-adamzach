1b62eed3d353ce02abd80e0045f0cbe8926cf429
adamzach <amzach0826@gmail.com>
2020-12-17 16:21:28 +0000

    Finished almost all of it, might change a few things later

--- App.java
+++ App.java
@@ -1,24 +1,57 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello! In this game, you have to guess the secret number! Let's start!");
-        int number1 = 7;
-        int guess1 = 0;
-        while (guess1 != number1) 
-            {
-              guess1 = readInt("Guess the number between 1-10: ");
-            if ((number1 <= -1)&&(number1 >= 11))
-            {
-              System.out.println("I said a number 1-10...");
-            }
-            else if (guess1 > number1) 
-                {
-                System.out.println("Too high! Try again!");
-                }
-            else if (guess1 < number1)
-                {
-                  System.out.print("Too low! Try Again!");
-                }   
-        System.out.println("Correct!");
-        }
+        int secretNumber;
+            secretNumber = (int) (Math.random() * 9 + 1);
+            Scanner keyboard = new Scanner(System.in);
+            int guess;
+            System.out.println("Hello! In this game, you have to guess the secret number!");
+            System.out.println("Ready? Let's begin...");
+            do {
+                  System.out.print("Guess a number 1-10: ");
+                  guess = keyboard.nextInt();
+                  if (guess == secretNumber)
+                        System.out.println("You guessed it! Congratulations! On to level 2!");
+                  else if ((guess < 0)||(guess > 10))
+                        System.out.println("I said a number 1-10...");
+                  else if (guess < secretNumber)
+                        System.out.println("Too low! Try again!");
+                  else if (guess > secretNumber)
+                        System.out.println("Too high! Try again!");
+            } while (guess != secretNumber);
+        int secretNumber2;
+            secretNumber2 = (int) (Math.random() * 99 + 1);
+            int guess2;
+            System.out.println("You made it to level 2! This time, you have to guess from a larger range!");
+            System.out.println("Ready? Let's begin...");
+            do {
+                  System.out.print("Guess a number 1-100: ");
+                  guess2 = keyboard.nextInt();
+                  if (guess2 == secretNumber2)
+                        System.out.println("You guessed it! Congratulations! On to level 3!");
+                  else if ((guess2 < 0)||(guess2 > 100))
+                        System.out.println("I said a number 1-100...");
+                  else if (guess2 < secretNumber2)
+                        System.out.println("Too small! Try again!");
+                  else if (guess2 > secretNumber2)
+                        System.out.println("Too high! Try again!");
+            } while (guess2 != secretNumber2);
+        int secretNumber3;
+            secretNumber3 = (int) (Math.random() * 999 + 1);
+            int guess3;
+            System.out.println("You made it to level 3! You have to guess from an even larger range!");
+            System.out.println("Ready? Let's begin...");
+            do {
+                  System.out.print("Guess a number 1-1000: ");
+                  guess3 = keyboard.nextInt();
+                  if (guess3 == secretNumber3)
+                        System.out.println("You guessed it! Congratulations! You have beat the game!");
+                  else if ((guess3 < 0)||(guess3 > 1000))
+                        System.out.println("I said a number 1-1000...");
+                  else if (guess3 < secretNumber3)
+                        System.out.println("Too small! Try again!");
+                  else if (guess3 > secretNumber3)
+                        System.out.println("Too high! Try again!");
+            } while (guess3 != secretNumber3);
     }
 }
